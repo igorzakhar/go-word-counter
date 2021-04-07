5c91a31324bb0b6ecfab71c2b993449c3c477dc0
Igor Zakharov <igorzakhar@gmail.com>
2021-04-07 19:57:37 +0300

    Add 'word' arg instead hardcode value

--- main.go
+++ main.go
@@ -1,100 +1,101 @@
 package main
 
 import (
-    "bufio"
-    "bytes"
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-    "sync"
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sync"
 )
 
 // структура для хранения результатов каждого запроса
 // включая кол-во вхождений строки "Go" в теле ответа
 type result struct {
-    url   string
-    count int
-    err   error
+	url   string
+	count int
+	err   error
 }
 
 func main() {
-    maxGoroutines := 5
-    resultChannel := make(chan *result)
-    done := make(chan bool)
+	maxGoroutines := 5
+	word := "Go"
+	resultChannel := make(chan *result)
+	done := make(chan bool)
 
-    go RunConcurrentTasks(done, resultChannel, maxGoroutines)
+	go RunConcurrentTasks(word, done, resultChannel, maxGoroutines)
 
-    go func() {
-        <-done
-        close(resultChannel)
-    }()
+	go func() {
+		<-done
+		close(resultChannel)
+	}()
 
-    OutputOfResults(resultChannel)
+	OutputOfResults(resultChannel)
 }
 
 // RunConcurrentTasks запускает параллельные задачи GetWordCount, число одновременно
 // выполняемыx задач ограничено величиной передаваемой в аргументе concurrencyLimit
-func RunConcurrentTasks(done chan bool, resultChan chan *result, concurrencyLimit int) {
-    var waitgroup sync.WaitGroup
-    semaphoreChan := make(chan struct{}, concurrencyLimit)
+func RunConcurrentTasks(word string, done chan bool, resultChan chan *result, concurrencyLimit int) {
+	var waitgroup sync.WaitGroup
+	semaphoreChan := make(chan struct{}, concurrencyLimit)
 
-    defer func() {
-        close(semaphoreChan)
-    }()
+	defer func() {
+		close(semaphoreChan)
+	}()
 
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    for scanner.Scan() {
-        semaphoreChan <- struct{}{}
-        waitgroup.Add(1)
+	for scanner.Scan() {
+		semaphoreChan <- struct{}{}
+		waitgroup.Add(1)
 
-        go func(url string) {
-            defer waitgroup.Done()
+		go func(url string) {
+			defer waitgroup.Done()
 
-            res := GetWordCount(url)
-            resultChan <- &res
+			res := GetWordCount(url, word)
+			resultChan <- &res
 
-            <-semaphoreChan
+			<-semaphoreChan
 
-        }(scanner.Text())
-    }
-    waitgroup.Wait()
-    done <- true
+		}(scanner.Text())
+	}
+	waitgroup.Wait()
+	done <- true
 }
 
 // GetWordCount отправляет http get запрос на указанный url-адрес и считает
 // кол-во вхождений строки "Go" в теле ответа, возвращает значение типа result
-func GetWordCount(url string) result {
-    response, err := http.Get(url)
-    if err != nil {
-        return result{url, 0, err}
-    }
-    defer response.Body.Close()
+func GetWordCount(url, word string) result {
+	response, err := http.Get(url)
+	if err != nil {
+		return result{url, 0, err}
+	}
+	defer response.Body.Close()
 
-    body, err := ioutil.ReadAll(response.Body)
-    if err != nil {
-        return result{url, 0, err}
-    }
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return result{url, 0, err}
+	}
 
-    wordCount := bytes.Count(body, []byte("Go"))
+	wordCount := bytes.Count(body, []byte(word))
 
-    return result{url, wordCount, err}
+	return result{url, wordCount, err}
 }
 
 // OutputOfResults слушает канал resultChan, при получении результата вычислений каждой задачи
 // GetWordCount выводит его в stdout, аккумулирует значение поля count экземпляра структуры result
 // в переменной totalCount и по окончании цикла чтения из канала выводит его в stdout
 func OutputOfResults(resultChan chan *result) {
-    totalCount := 0
+	totalCount := 0
 
-    for res := range resultChan {
-        if res.err != nil {
-            continue
-        }
-        fmt.Printf("Count for %s: %d\n", res.url, res.count)
+	for res := range resultChan {
+		if res.err != nil {
+			continue
+		}
+		fmt.Printf("Count for %s: %d\n", res.url, res.count)
 
-        totalCount += res.count
-    }
-    fmt.Printf("Total: %v\n", totalCount)
+		totalCount += res.count
+	}
+	fmt.Printf("Total: %v\n", totalCount)
 }
